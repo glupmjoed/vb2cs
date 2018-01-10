208a1057eb58df366865f948c4e96eb322c7022d
Phillip Alexander <glupmjoed@fafnir.dk>
2018-01-10 19:10:26 +0100

    Add example of VB array declaration

---
+++ vb_examples/dim_array.vb
@@ -0,0 +1,11 @@
+Module DimArray
+
+    Sub Main()
+        Dim array(9) As Integer
+        Dim elem As Integer
+        For Each elem In array
+            Console.WriteLine(elem)
+        Next
+    End Sub
+
+End Module
