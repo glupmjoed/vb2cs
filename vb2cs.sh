8e1ce4a23a42e9df6745b05e9e3f082d1af2b4fc
Phillip Alexander <glupmjoed@fafnir.dk>
2018-01-14 07:19:25 +0100

    Add flag to output CRLF newlines

--- vb2cs.sh
+++ vb2cs.sh
@@ -2,6 +2,18 @@
 
 cli=$(find CodeConverterCLI/bin/Debug/ | grep 'CodeConverterCLI$' | head -1)
 
+# Parse flags
+while getopts ":r" opt; do
+    case ${opt} in
+        r ) rflag=1 ;;
+        \? ) echo "Usage: $0 [-r]" ;;
+    esac
+done
+shift $((OPTIND - 1))
+
 if [ $1 ]; then ./pre.sed $1; else ./pre.sed; fi |
 
-    ./$cli | ./post.sed
+    ./$cli |
+
+    # Postprocess generated code. Change newlines to \r\n if -r flag is set
+    if [ $rflag ]; then ./post.sed | sed 's/$/\r/g'; else ./post.sed; fi
