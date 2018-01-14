1c29b9ad985be42bce94c4745c040ff73608fc72
Phillip Alexander <glupmjoed@fafnir.dk>
2018-01-14 12:50:29 +0100

    Rename variable and add error handling for missing binary

--- vb2cs.sh
+++ vb2cs.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-cli=$(find CodeConverterCLI/bin/Debug/ | grep 'CodeConverterCLI$' | head -1)
-
 # Parse flags
 while getopts ":r" opt; do
     case ${opt} in
@@ -11,9 +9,18 @@ while getopts ":r" opt; do
 done
 shift $((OPTIND - 1))
 
+# Locate CodeConverterCLI binary
+# Exit if the command wasn't found
+CCCLI=$(find CodeConverterCLI/bin/Debug/ | grep 'CodeConverterCLI$' | head -1)
+if [ -z $CCCLI ]
+then
+    echo "Couldn't find command CodeConverterCLI" 1>&2
+    exit 1
+fi
+
 if [ $1 ]; then ./pre.sed $1; else ./pre.sed; fi |
 
-    ./$cli |
+    ./$CCCLI |
 
     # Postprocess generated code. Change newlines to \r\n if -r flag is set
     if [ $rflag ]; then ./post.sed | sed 's/$/\r/g'; else ./post.sed; fi
