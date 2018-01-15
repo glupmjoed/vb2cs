cab45d40901e7dbd077da533e22e9fc535ee81cb
Phillip Alexander <glupmjoed@fafnir.dk>
2018-01-15 16:04:56 +0100

    Add -b flag and rearrange handling of default binary

--- vb2cs.sh
+++ vb2cs.sh
@@ -1,26 +1,34 @@
 #!/bin/bash
 
+BNAME=CodeConverterCLI
+
 # Parse flags
-while getopts ":r" opt; do
+while getopts ":b:r" opt; do
     case ${opt} in
+        b ) CCCLI=$OPTARG ;;
         r ) rflag=1 ;;
-        \? ) echo "Usage: $0 [-r]" ;;
+        \? ) echo "Usage: $0 [-b FILE] [-r]" 1>&2
+             exit 1 ;;
     esac
 done
 shift $((OPTIND - 1))
 
 # Locate CodeConverterCLI binary
-# Exit if the command wasn't found
-CCCLI=$(find CodeConverterCLI/bin/Debug/ | grep 'CodeConverterCLI$' | head -1)
 if [ -z $CCCLI ]
 then
-    echo "Couldn't find command CodeConverterCLI" 1>&2
-    exit 1
+    CCCLI=$(find $BNAME/bin/Debug/ |grep "$BNAME\$" | head -1)
+
+    # Exit if the binary wasn't found
+    if [ -z $CCCLI ]
+    then
+        echo "Couldn't find command $BNAME" 1>&2
+        exit 1
+    fi
 fi
 
 if [ $1 ]; then ./pre.sed $1; else ./pre.sed; fi |
 
-    ./$CCCLI |
+    $CCCLI |
 
     # Postprocess generated code. Change newlines to \r\n if -r flag is set
     if [ $rflag ]; then ./post.sed | sed 's/$/\r/g'; else ./post.sed; fi
