2308fdfbcf6c75db019771dbb7ead3c0ae3ccfe4
Phillip Alexander <glupmjoed@fafnir.dk>
2018-01-18 23:34:27 +0100

    Add function conversions for libraries with ambiguous import paths

--- func_conv.sh
+++ func_conv.sh
@@ -17,9 +17,24 @@ DIR_PATH_O="Assembly.GetExecutingAssembly().Location"
 FILE_EXISTS_I="My.Computer.FileSystem.FileExists"
 FILE_EXISTS_O="System.IO.File.Exists"
 
+# Translation variables for libraries with ambiguous import paths
+
+XDOC_I="XDocument"
+XDOC_O="System.Xml.Linq.XDocument"
+
+XELEM_I="XElement"
+XELEM_O="System.Xml.Linq.XElement"
+
+# Replace all occurences of *_I names with *_O:
+
 sed -E "
+
 s/\b$CREAT_DIR_I\b/$CREAT_DIR_O/g
 s/\b$DEL_FILE_I\b/$DEL_FILE_O/g
 s/\b$DIR_PATH_I\b/$DIR_PATH_O/g
 s/\b$FILE_EXISTS_I\b/$FILE_EXISTS_O/g
+
+s/\b$XDOC_I\b/$XDOC_O/g
+s/\b$XELEM_I\b/$XELEM_O/g
+
 "
